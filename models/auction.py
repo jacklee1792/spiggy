b0b1bbe98153f578de1afe60748e1834988d06a0
Jack Lee <jacklee1792@gmail.com>
2021-05-04 02:57:14 -0400

    Add active auction checker, experiment with gap flipping

--- models/auction.py
+++ models/auction.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timezone
 from typing import Any, Dict
 
-from item import Item, make_item
-from user import User
+from models.item import Item, make_item
+from models.user import User
 
 
 class Auction:
@@ -40,7 +40,8 @@ class EndedAuction(Auction):
         self.auction_id = d['auction_id']
         self.seller = User(d['seller'])
         self.is_bin = d['bin']
-        self.end_time = datetime.fromtimestamp(d['timestamp'], tz=timezone.utc)
+        self.end_time = datetime.fromtimestamp(d['timestamp'] / 1000,
+                                               tz=timezone.utc)
         self.price = d['price']
         self.item = make_item(d['item_bytes'])
 
@@ -67,8 +68,27 @@ class ActiveAuction(Auction):
         self.auction_id = d['uuid']
         self.seller = User(d['auctioneer'])
         self.is_bin = 'bin' in d
-        self.end_time = datetime.fromtimestamp(d['end'], tz=timezone.utc)
+        self.end_time = datetime.fromtimestamp(d['end'] / 1000, tz=timezone.utc)
         self.price = d['highest_bid_amount']
         self.item = make_item(d['item_bytes'])
         self.starting_price = d['starting_bid']
 
+
+if __name__ == '__main__':
+    # uhhh why doesn't my item import work
+    d = {
+        'uuid': 'asdf',
+        'auctioneer': 'asdf',
+        'bin': True,
+        'end': 1620101495396,
+        'highest_bid_amount': 123123,
+        'item_bytes': (
+            'H4sIAAAAAAAAACWPwUrDQBRFb9JqkxEUN4K76U8oLpOmolCKtN3LSzOkY6cZybxA+0'
+            'X5j3yBXyS+4OrB4ZwLTwEpIqsARDFiWyU/Ea4Wvms4Upgw1ROkb7Yyr47qINavwqyy'
+            '4dvRJcV05VuTCJ1hPvTPH472RvPBBm2bIJEm/dUdTenPmj0eRBk7oSUxO6ODb+q55I'
+            '9D/5Q7Ciydzqlh08oZFdxrLVl+0duDbY5+bVh8NfQvQ+822WaZYLqmk8GtoP9EFzbs'
+            'oXC3PHNLGXNry45NSMb3cJNnu91q+Vm8bxdAjOuCTlQbGcUfet7RKAwBAAA='
+        ),
+        'starting_bid': 123
+    }
+    a = ActiveAuction(d)
